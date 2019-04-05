7a535c8dc13ee85d8306130a851145b8305c2462
liu22 <liu2272772@163.com>
2019-04-05 13:32:18 +0800

    添加findOne方法

---
+++ src/main/java/org/shtl/mybatis/generator/impl/CustomJavaMapperMethodGenerator.java
@@ -0,0 +1,58 @@
+package org.shtl.mybatis.generator.impl;
+
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 添加方法
+ *
+ * @author Zhaolong Liu
+ * @create 2019-04-05
+ */
+public class CustomJavaMapperMethodGenerator extends AbstractJavaMapperMethodGenerator {
+
+    //方法名称
+    private static final String FIND_ONE = "finOne";
+
+    @Override
+    public void addInterfaceElements(Interface anInterface) {
+        //添加findOne
+        addInterfaceFindOne(anInterface);
+    }
+
+    /**
+     * 添加findOne方法
+     *
+     * @param anInterface Interface
+     */
+    private void addInterfaceFindOne(Interface anInterface) {
+        //创建import对象
+        Set<FullyQualifiedJavaType> importedTypes = new TreeSet<>();
+        //添加List包
+        importedTypes.add(FullyQualifiedJavaType.getNewListInstance());
+        //添加类型对象
+        FullyQualifiedJavaType returnType = new FullyQualifiedJavaType(introspectedTable.getBaseRecordType());
+        importedTypes.add(returnType);
+
+        //方法对象
+        Method method = new Method();
+        method.setVisibility(JavaVisibility.PUBLIC);
+        method.setReturnType(returnType);//设置放回类型对象
+        method.setName(FIND_ONE);//设置方法名称
+        //添加参数
+        method.addParameter(new Parameter(returnType, "record"));
+
+        addMapperAnnotations(anInterface, method);
+        context.getCommentGenerator().addGeneralMethodComment(method, introspectedTable);
+        if (context.getPlugins().clientSelectByPrimaryKeyMethodGenerated(method, anInterface, introspectedTable)) {
+            anInterface.addImportedTypes(importedTypes);
+            anInterface.addMethod(method);
+        }
+    }
+
+    public void addMapperAnnotations(Interface interfaze, Method method) {
+    }
+}
---
+++ src/main/java/org/shtl/mybatis/generator/plugin/CustomPlugin.java
@@ -0,0 +1,45 @@
+package org.shtl.mybatis.generator.plugin;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.Document;
+import org.mybatis.generator.codegen.mybatis3.javamapper.elements.AbstractJavaMapperMethodGenerator;
+import org.mybatis.generator.codegen.mybatis3.xmlmapper.elements.AbstractXmlElementGenerator;
+import org.shtl.mybatis.generator.impl.CustomAbstractXmlElementGenerator;
+import org.shtl.mybatis.generator.impl.CustomJavaMapperMethodGenerator;
+
+import java.util.List;
+
+/**
+ * 自定义方法
+ *
+ * @author Zhaolong Liu
+ * @create 2019-04-05
+ */
+public class CustomPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
+        AbstractXmlElementGenerator elementGenerator = new CustomAbstractXmlElementGenerator();
+        elementGenerator.setContext(context);
+        elementGenerator.setIntrospectedTable(introspectedTable);
+        elementGenerator.addElements(document.getRootElement());
+        return super.sqlMapDocumentGenerated(document, introspectedTable);
+    }
+
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        AbstractJavaMapperMethodGenerator methodGenerator = new CustomJavaMapperMethodGenerator();
+        methodGenerator.setContext(context);
+        methodGenerator.setIntrospectedTable(introspectedTable);
+        methodGenerator.addInterfaceElements(interfaze);
+        return super.clientGenerated(interfaze, topLevelClass, introspectedTable);
+    }
+}
